2634c87219444efa0d009e097f1641d56114403a
HughG-50 <hugh.greethead@gmail.com>
2020-06-18 15:57:25 +1000

    Basic styling functionality for board table grid

---
+++ main.js
@@ -0,0 +1,31 @@
+
+// --- constants --- 
+
+// initialize board to empty 2D array
+let board = [
+    [],
+    [],
+    []
+]
+
+// app's state (variables) 
+
+
+// cached element references 
+
+
+// --- event listeners ---
+
+
+// --- functions --- 
+
+// set initial state of game board 
+function initBoard() {
+    board = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+    ]
+}
+
+initBoard()
